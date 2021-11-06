1e10b91e78a50dfa208c4ddabc2f776c69575d20
Kotomi <piitun3232@gmail.com>
2021-11-06 22:55:42 +0900

    styled-componentsいれた

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-  return <h1>Hello React!</h1>
+  return (
+    <div>
+      <header>
+        <div>Kanban board</div>
+
+        <input placeholder="Filter cards" />
+      </header>
+
+      <div>
+        <section>
+          <h3>TODO</h3>
+
+          <article>朝食をとる</article>
+          <article>歯を磨く</article>
+          <article>ランニング</article>
+        </section>
+
+        <section>
+          <h3>Doing</h3>
+
+          <article>プログラミング</article>
+          <article>風呂に入る</article>
+        </section>
+
+        <section>
+          <h3>Waiting</h3>
+        </section>
+
+        <section>
+          <h3>Done</h3>
+
+          <article>布団から出る (:3っ)っ -=三[＿＿]</article>
+        </section>
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
