88b01148e49677fcac30a6c793a42bcb06566359
kotomi.satou <piitun3232@gmail.com>
2021-11-21 16:38:20 +0900

    card並び替え実装

--- src/Card.tsx
+++ src/Card.tsx
@@ -16,18 +16,18 @@ export function Card({
 }) {
   const [drag, setDrag] = useState(false)
 
-  return (
-    <Container
-      style={{ opacity: drag ? 0.5 : undefined }}
-      onDragStart={() => {
-        onDragStart?.()
-        setDrag(true)
-      }}
-      onDragEnd={() => {
-        onDragEnd?.()
-        setDrag(false)
-      }}
-    >
+ return (
+     <Container
+       style={{ opacity: drag ? 0.5 : undefined }}
+       onDragStart={() => {
+         onDragStart?.()
+         setDrag(true)
+       }}
+       onDragEnd={() => {
+         onDragEnd?.()
+         setDrag(false)
+       }}
+     >
       <CheckIcon />
 
       {text?.split(/(https?:\/\/\S+)/g).map((fragment, i) =>
@@ -97,93 +97,93 @@ const Link = styled.a.attrs({
 `
 
 function DropArea({
-  disabled,
-  onDrop,
-  children,
-  className,
-  style,
-}: {
-  disabled?: boolean
-  onDrop?(): void
-  children?: React.ReactNode
-  className?: string
-  style?: React.CSSProperties
-  }) {
-  const [isTarget, setIsTarget] = useState(false)
-  const visible = !disabled && isTarget
-
-  const [dragOver, onDragOver] = useDragAutoLeave()
-
-  return (
-    <DropAreaContainer
-      style={style}
-      className={className}
-      onDragOver={ev => {
-        if (disabled) return
-        
-        ev.preventDefault()
-        onDragOver(() => setIsTarget(false))
-      }}
-      onDragEnter={() => {
-        if (disabled || dragOver.current) return
-        
-        setIsTarget(true)
-      }}
-      onDrop={() => {
-        if (disabled) return
-        
-        setIsTarget(false)
-        onDrop?.()
-      }}
-    >
-      <DropAreaIndicator
-        style={{
-          height: !visible ? 0 : undefined,
-          borderWidth: !visible ? 0 : undefined,
-        }}
-      />
-
-      {children}
-    </DropAreaContainer>
-  )
-}
-
-/**
+   disabled,
+   onDrop,
+   children,
+   className,
+   style,
+ }: {
+   disabled?: boolean
+   onDrop?(): void
+   children?: React.ReactNode
+   className?: string
+   style?: React.CSSProperties
+ }) {
+   const [isTarget, setIsTarget] = useState(false)
+   const visible = !disabled && isTarget
+ 
+   const [dragOver, onDragOver] = useDragAutoLeave()
+ 
+   return (
+     <DropAreaContainer
+       style={style}
+       className={className}
+       onDragOver={ev => {
+         if (disabled) return
+ 
+         ev.preventDefault()
+         onDragOver(() => setIsTarget(false))
+       }}
+       onDragEnter={() => {
+         if (disabled || dragOver.current) return
+ 
+         setIsTarget(true)
+       }}
+       onDrop={() => {
+         if (disabled) return
+ 
+         setIsTarget(false)
+         onDrop?.()
+       }}
+     >
+       <DropAreaIndicator
+         style={{
+           height: !visible ? 0 : undefined,
+           borderWidth: !visible ? 0 : undefined,
+         }}
+       />
+ 
+       {children}
+     </DropAreaContainer>
+   )
+ }
+ 
+ /**
   * dragOver イベントが継続中かどうかのフラグを ref として返す
   *
   * timeout 経過後に自動でフラグが false になる
   *
   * @param timeout 自動でフラグを false にするまでの時間 (ms)
   */
-function useDragAutoLeave(timeout: number = 100) {
-  const dragOver = useRef(false)
-  const timer = useRef(0)
-
-  return [
-    dragOver,
-
-    /**
+ function useDragAutoLeave(timeout: number = 100) {
+   const dragOver = useRef(false)
+   const timer = useRef(0)
+ 
+   return [
+     dragOver,
+ 
+     /**
       * @param onDragLeave フラグが false になるときに呼ぶコールバック
       */
-    (onDragLeave?: () => void) => {
-      clearTimeout(timer.current)
-
-      dragOver.current = true
-      timer.current = setTimeout(() => {
-        dragOver.current = false
-        onDragLeave?.()
-      }, timeout)
-    },
-  ] as const
-}
+     (onDragLeave?: () => void) => {
+       clearTimeout(timer.current)
  
-const DropAreaContainer = styled.div`
-  > :not(:first-child) {
-    margin-top: 8px;
-  }
-`
-
-const DropAreaIndicator = styled.div`
+       dragOver.current = true
+       timer.current = setTimeout(() => {
+         dragOver.current = false
+         onDragLeave?.()
+       }, timeout)
+     },
+   ] as const
+ }
+ 
+ const DropAreaContainer = styled.div`
+   > :not(:first-child) {
+     margin-top: 8px;
+   }
+ `
+ 
+ const DropAreaIndicator = styled.div`
    height: 40px;
    border: dashed 3px ${color.Gray};
    border-radius: 6px;
